2e20df289b89e3c4d037b0d245f3188086f91c7a
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-04-08 21:37:34 +0200

    Developed project edit page, lacks backend implementation

--- Client side/src/App.js
+++ Client side/src/App.js
@@ -13,6 +13,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import user from "./lib/user-service";
 import NewProject from "./pages/NewProject";
 import ProjectDetails from "./pages/ProjectDetails";
+import ProjectEdit from "./pages/ProjectEdit";
 
 class App extends Component {
   render() {
@@ -25,6 +26,7 @@ class App extends Component {
             <AnonRoute exact path='/login' component={Login} />
             <PrivateRoute exact path='/project/new' component={NewProject}/>
             <PrivateRoute exact path='/project/details' component={ProjectDetails} id=""/>
+            <PrivateRoute exact path='/project/edit' component={ProjectEdit} id=""/>
           </Switch>
         </div>
       </AuthProvider>
---
+++ Client side/src/pages/ProjectEdit.js
@@ -0,0 +1,335 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withAuth } from "../lib/AuthProvider";
+import userService from "../lib/user-service";
+
+class NewProject extends Component {
+  state = {
+    name: "",
+    userList: [],
+    type: "",
+    client: "",
+    description: "",
+    teamMembers: [],
+    budgetNumber: "",
+    id: "5e8e03ae22240853db5be3cc",
+    teamNames:[],
+  };
+
+  componentDidMount = async () => {
+    let userList = await userService.getUsers();
+    // let id = this.props.projectId
+    let project = await userService.getProject("5e8e03ae22240853db5be3cc");
+    let teamMembersInOrder= [];
+    let teamMembersNames= [];
+    project.teamMembers.map(member=> {
+        let index;
+        switch (member.role) {
+          case "Account":
+            teamMembersInOrder[0]=member._id;
+            teamMembersNames[0]=member.name;
+            break;
+          case "Scientific":
+            teamMembersInOrder[1]=member._id;
+            teamMembersNames[1]=member.name;
+            break;
+          case "Design":
+            teamMembersInOrder[2]=member._id;
+            teamMembersNames[2]=member.name;
+            break;
+          case "Developer":
+            teamMembersInOrder[3]=member._id;
+            teamMembersNames[3]=member.name;
+            break;
+          case "AV":
+            teamMembersInOrder[4]=member._id;
+            teamMembersNames[4]=member.name;
+            break;
+          case "Administration":
+            teamMembersInOrder[5]=member._id;
+            teamMembersNames[5]=member.name;
+            break;
+        }
+    })
+    this.setState({
+      userList: userList,
+      name: project.name,
+      budgetNumber: project.budgetNumber,
+      type: project.type,
+      client: project.client,
+      description: project.description,
+      teamMembers: teamMembersInOrder,
+      teamNames: teamMembersNames,
+    });
+  };
+
+  
+
+
+
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const {
+      name,
+      budgetNumber,
+      client,
+      description,
+      type,
+      teamMembers,
+    } = this.state;
+    userService.newProject({
+      name,
+      budgetNumber,
+      client,
+      description,
+      type,
+      teamMembers,
+    });
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    let teamMembers = this.state.teamMembers;
+    let index;
+    switch (name) {
+      case "Account":
+        index = 0;
+        break;
+      case "Scientific":
+        index = 1;
+        break;
+      case "Design":
+        index = 2;
+        break;
+      case "Developer":
+        index = 3;
+        break;
+      case "AV":
+        index = 4;
+        break;
+      case "Administration":
+        index = 5;
+        break;
+    }
+    teamMembers[index] = value;
+    this.setState({
+      [name]: value,
+      teamMembers: teamMembers,
+    });
+  };
+
+  render() {
+    const { name, budgetNumber, client, description } = this.state;
+    let member = [];
+
+    for (let i = 0; i < 6; i++) {
+      if (this.state.teamMembers[i]) {
+        member.push(
+          <option
+            value={this.state.teamMembers[i]}
+            className="font-weight-bold"
+          >
+            Currently: {this.state.teamNames[i]}
+          </option>
+        );
+      } else {
+        member.push(<option value="">Select a person</option>);
+      }
+    }
+
+    return (
+      <div className="mt-5">
+        <h1>Create new project</h1>
+
+        <form onSubmit={this.handleFormSubmit} className="d-flex flex-column">
+          <div className="row">
+            <div className="col-12">
+              <label>Name:</label>
+              <input
+                className="w-100"
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="col-4">
+              <label>Budget number</label>
+              <input
+                className="w-100"
+                type="text"
+                name="budgetNumber"
+                value={budgetNumber}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="col-4">
+              <label>Client:</label>
+              <input
+                className="w-100"
+                type="text"
+                name="client"
+                value={client}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="col-4">
+              <label>Project type</label>
+              <select
+                className="w-100"
+                name="type"
+                onChange={this.handleChange}
+                required
+              >
+                <option value={this.state.type} className="font-weight-bold">
+                  Currently: {this.state.type}
+                </option>
+                <option value="Leaflet">Leaflet</option>
+                <option value="Slidekit">Slidekit</option>
+                <option value="eDetailing">eDetailing</option>
+                <option value="Website">Website</option>
+                <option value="Event">Event</option>
+                <option value="Video">Video</option>
+              </select>
+            </div>
+          </div>
+
+          <h3>Project team members:</h3>
+          <div className="row">
+            <div className="col-4">
+              <label>Account:</label>
+              <select
+                className="w-100"
+                name="Account"
+                onChange={this.handleChange}
+                required
+              >
+                {member[0]}
+                {this.state.userList.map((user) => {
+                  if (user.role === "Account") {
+                    return (
+                      <option key={user._id} value={user._id}>
+                        {user.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+            <div className="col-4">
+              <label>Scientific:</label>
+              <select
+                className="w-100"
+                name="Scientific"
+                onChange={this.handleChange}
+              >
+                {member[1]}
+                {this.state.userList.map((user) => {
+                  if (user.role === "Scientific") {
+                    return (
+                      <option key={user._id} value={user._id}>
+                        {user.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+            <div className="col-4">
+              <label>Design:</label>
+              <select
+                className="w-100"
+                name="Design"
+                onChange={this.handleChange}
+              >
+                {member[2]}
+                {this.state.userList.map((user) => {
+                  if (user.role === "Design") {
+                    return (
+                      <option key={user._id} value={user._id}>
+                        {user.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+            <div className="col-4">
+              <label>Developer:</label>
+              <select
+                className="w-100"
+                name="Developer"
+                onChange={this.handleChange}
+              >
+                {member[3]}
+                {this.state.userList.map((user) => {
+                  if (user.role === "Developer") {
+                    return (
+                      <option key={user._id} value={user._id}>
+                        {user.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+            <div className="col-4">
+              <label>Audiovisual:</label>
+              <select className="w-100" name="AV" onChange={this.handleChange}>
+                {member[4]}
+                {this.state.userList.map((user) => {
+                  if (user.role === "AV") {
+                    return (
+                      <option key={user._id} value={user._id}>
+                        {user.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+            <div className="col-4">
+              <label>Administration:</label>
+              <select
+                className="w-100"
+                name="Administration"
+                onChange={this.handleChange}
+              >
+                {member[5]}
+                {this.state.userList.map((user) => {
+                  if (user.role === "Administration") {
+                    return (
+                      <option key={user._id} value={user._id}>
+                        {user.name}
+                      </option>
+                    );
+                  }
+                })}
+              </select>
+            </div>
+            <div className="col-12">
+              <h3>Description:</h3>
+              <textarea
+                className="w-100"
+                type="text"
+                name="description"
+                onChange={this.handleChange}
+              >
+                {description}
+              </textarea>
+            </div>
+          </div>
+
+          <input
+            type="submit"
+            className="my-5 btn btn-success p-2 font-weight-bold"
+            value="Create the project"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withAuth(NewProject);
