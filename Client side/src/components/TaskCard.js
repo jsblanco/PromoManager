e4daa415a288d3bf75787a96df0f59751814c078
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-04-10 15:44:35 +0200

    Added conditionals to task card to show different input to each team member

--- Client side/src/components/TaskCard.js
+++ Client side/src/components/TaskCard.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import userService from "../lib/user-service";
+import { withAuth } from "../lib/AuthProvider";
 
-export default class TaskCard extends Component {
+class TaskCard extends Component {
   state = {
     projectId: this.props.projectId,
     teamMembers: this.props.teamMembers,
@@ -12,15 +13,11 @@ export default class TaskCard extends Component {
     assignedUser: this.props.task.assignedUser,
     deadline: this.props.task.deadline,
     showButton: false,
+    taskNotOk: false,
   };
 
   handleChange = (event) => {
     let { name, value } = event.target;
-    // if (name==="deadline"){value =
-
-    //   Date.parse(value)
-
-    // }
     this.setState({
       [name]: value,
       showButton: true,
@@ -43,13 +40,18 @@ export default class TaskCard extends Component {
     });
   };
 
-  parseDate(HTMLdate) {
-    var b = HTMLdate.split(/\D/);
-    return new Date(b[0], --b[1], b[2]);
-  }
+  showMessageInput = () => {
+    this.setState({
+      taskNotOk: !this.state.taskNotOk,
+    });
+  };
 
   render() {
-    let button, deadlineInput, deadline, assignedUser;
+    let button,
+      deadlineInput,
+      taskInformation,
+      completeTaskButtons,
+      messageInput;
     if (this.state.showButton) {
       button = (
         <button className="btn btn-warning" type="submit">
@@ -75,23 +77,22 @@ export default class TaskCard extends Component {
     } else {
       deadlineInput = (
         <div className="col-5">
-        <label htmlFor="deadline" className="pr-3 text-danger" >
-          Assign a deadline:
-        </label>
-        <input type="date" name="deadline" onChange={this.handleChange} value={this.state.deadline} required/>
-      </div>
+          <label htmlFor="deadline" className="pr-3 text-danger">
+            Assign a deadline:
+          </label>
+          <input
+            type="date"
+            name="deadline"
+            onChange={this.handleChange}
+            value={this.state.deadline}
+            required
+          />
+        </div>
       );
     }
 
-    
-
-    
-    return (
-      <div className="my-1 card px-2">
-        <h5 className="pt-3">
-          <b>Task: </b>
-          {this.state.task.name}
-        </h5>
+    if (this.props.user.role === "Account") {
+      taskInformation = (
         <form onSubmit={this.updateTask} className="row py-2">
           <div className="col-5">
             <label htmlFor="assignedUser" className="pr-3">
@@ -115,10 +116,74 @@ export default class TaskCard extends Component {
             </select>
           </div>
           {deadlineInput}
+          <div className="col-2 float-right">{button}</div>
+        </form>
+      );
+    } else {
+      taskInformation = (
+        <form onSubmit={this.updateTask} className="row py-2">
+          <div className="col-5">
+            <label className="pr-3">Assigned to:</label>
+            <p className="font-weight-bold d-inline">
+              {this.state.assignedUserName}
+            </p>
+          </div>
+
+          <div className="col-5">
+            <label className="pr-3">Deadline:</label>
+            <p className="font-weight-bold d-inline">
+              {this.state.task.deadline}
+            </p>
+          </div>
 
           <div className="col-2 float-right">{button}</div>
         </form>
+      );
+    }
+
+    if (this.props.user._id === this.state.task.assignedUser) {
+      completeTaskButtons = (
+        <div className="row justify-content-center mb-2">
+          <button className="btn btn-success mx-2">Task completed</button>
+          <button
+            onClick={this.showMessageInput}
+            className="btn btn-danger mx-2"
+          >
+            Issue detected
+          </button>
+
+        </div>
+      );
+    }
+
+    if (this.state.taskNotOk === true) {
+      messageInput = (
+        <div className="row mb-2">
+          <div className="col-8">
+            <input type="text" name="message" className="w-100 pt-1 pb-2" placeholder="Describe the problem to your colleague" required />
+          </div>
+          <div className="col-4">
+            <button className="btn btn-danger">
+              Return to previous team member
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    ////////////////////////////
+    return (
+      <div className="my-1 card px-2">
+        <h5 className="pt-3">
+          <b>Task: </b>
+          {this.state.task.name}
+        </h5>
+        {taskInformation}
+        {completeTaskButtons}
+        {messageInput}
       </div>
     );
   }
 }
+
+export default withAuth(TaskCard);
